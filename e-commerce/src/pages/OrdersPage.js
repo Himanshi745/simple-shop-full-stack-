56d1bc99f7049b40f5547cf156bf140b84f59eda
Himanshi745 <166390857+Himanshi745@users.noreply.github.com>
2025-07-10 12:50:45 +0530

    Update OrdersPage.js

--- e-commerce/src/pages/OrdersPage.js
+++ e-commerce/src/pages/OrdersPage.js
@@ -5,13 +5,7 @@ const OrdersPage = ({ cartItems, handlePlaceOrder }) => {
   return (
     <div className="orders-page" style={{ padding: "2rem" }}>
       <h2>Your Orders</h2>
-      {cartItems.length > 0 ? (
-        <button onClick={handlePlaceOrder} className="place-order-btn">
-          Place Order
-        </button>
-      ) : (
-        <p>No items in cart to order.</p>
-      )}
+      
     </div>
   );
 };
