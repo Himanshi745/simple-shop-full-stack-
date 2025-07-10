5655d790f61ba2e76f94838bf8c6cf5bb503053b
Himanshi745 <166390857+Himanshi745@users.noreply.github.com>
2025-07-10 10:37:34 +0530

    Update App.js

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React, { useRef, useState, useCallback, useEffect } from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   useLocation,
