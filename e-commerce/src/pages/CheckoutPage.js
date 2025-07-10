6adabf74c9f4114c35121542ef71bcc9542e1cd9
Himanshi745 <166390857+Himanshi745@users.noreply.github.com>
2025-07-10 11:29:13 +0530

    Update CheckoutPage.js

--- e-commerce/src/pages/CheckoutPage.js
+++ e-commerce/src/pages/CheckoutPage.js
@@ -32,7 +32,7 @@ const CheckoutPage = () => {
 
     try {
       setIsPlacingOrder(true);
-      await axios.post("https://simple-shop-full-stack.onrender.com/", {
+      await axios.post("https://simple-shop-full-stack.onrender.com/order", {
         items: [product],
         address,
         phone,
